5b5dc3e761efe7a8db99fea683fc57fa35680655
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-05 14:52:06 +0200

    spring init

---
+++ src/main/java/com/example/security_lab/SecurityLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/security_lab/SecurityLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
